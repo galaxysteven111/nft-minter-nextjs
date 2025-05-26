29cde22eb2cc58916d2b07c2d377460685cef9b8
galaxysteven111 <galaxysteven85@gmail.com>
2025-05-27 01:14:30 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,13 @@
-// pages/_app.tsx（Page Router 用法）
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { clusterApiUrl } from '@solana/web3.js'
+import '@solana/wallet-adapter-react-ui/styles.css'
+import '../styles/globals.css'
 
-require('@solana/wallet-adapter-react-ui/styles.css')
-
-const wallets = [new PhantomWalletAdapter()]
 const endpoint = clusterApiUrl('devnet')
+const wallets = [new PhantomWalletAdapter()]
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
